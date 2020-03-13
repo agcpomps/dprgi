90143352c76dfd0926ba203f08386e277f9b3677
agcpomps <agcpomps@gmail.com>
2020-03-13 14:55:34 +0100

    models habitacao e meia customização do admin

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import Morador
+from . models import Morador, Habitacao
 
 admin.site.register(Morador)
+admin.site.register(Habitacao)
--- cadastro/models.py
+++ cadastro/models.py
@@ -13,7 +13,7 @@ class Morador(models.Model):
     funcionario_publico = models.BooleanField(default=False)
     local_de_trabalho = models.CharField(max_length=200)
     salario_base = models.DecimalField(max_digits=6, decimal_places=2)
-    num_agregado_familiar = models.ImageField()
+    num_agregado_familiar = models.BooleanField()
     num_habit_estado = models.IntegerField
 
     @property
@@ -25,4 +25,66 @@ class Morador(models.Model):
     
     class Meta:
         verbose_name_plural = 'moradores'
+
+class Habitacao(models.Model):
+
+    PUBLICA = 'PU'
+    PUBLICA_PRIVADA = 'PP'
+    PRIVADA = 'PR'
+    OUTROS = 'OU'
+
+    iniciativa_escolhas = [
+        (PUBLICA, 'pública'),
+        (PUBLICA_PRIVADA, 'pública-privada'),
+        (PRIVADA, 'privada'),
+        (OUTROS, 'outros')
+    ]
+    TIPO_T1 = 'T1'
+    TIPO_T2 = 'T2'
+    TIPO_T3 = 'T3'
+    TIPO_T4 = 'T4'
+
+    tipologia_escolhas = [
+        (TIPO_T1, 'tipologia T1'),
+        (TIPO_T2, 'tipologia T2'),
+        (TIPO_T3, 'tipologia T3'),
+        (TIPO_T4, 'tipologia T4'),
+    ]
+
+    morador = models.ForeignKey(Morador, on_delete=models.CASCADE)
+    numero_do_apartamento = models.CharField(max_length=100)
+    identificacao_do_projecto = models.CharField(max_length=200)
+    localizacao = models.CharField(max_length=200)
+    iniciativa = models.CharField(
+        max_length=2,
+        choices=iniciativa_escolhas,
+        default=PUBLICA
+    )
+    tipologia = models.CharField(
+        max_length=2,
+        choices=tipologia_escolhas,
+        default=TIPO_T2
+    )
+    
+    COMPRA = 'CP'
+    RENDA = 'RE'
+    estado_legal_escolhas = [
+        (COMPRA, 'Compra'),
+        (RENDA, 'Renda')
+    ]
+
+    andar = models.IntegerField()
+    numero_de_compartimentos = models.IntegerField()
+    estado_legal = models.CharField(
+        max_length=2,
+        choices=estado_legal_escolhas
+    )
+
+    alienado_ao_inh = models.BooleanField(default=True)
+
+    def __str__(self):
+        return numero_do_apartamento
+
+    class Meta:
+        verbose_name_plural = 'habitações'
 
